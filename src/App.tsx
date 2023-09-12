79c79b8098bdc1d5242cc671b38c0bdd5a6ae96d
Ariff <ariff@regovtech.com>
2023-09-12 08:42:04 +0800

    create sign-in + sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import "./App.css";
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <>
-      <h1>To Do App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/sign-in" />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </Router>
     </>
   );
 }
