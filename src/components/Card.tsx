13696036cdc9b0f1120ca83f295541f261a42bbe
Ariff <ariff@regovtech.com>
2023-09-12 12:33:28 +0800

    create card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: #48494b;
+  color: #333;
+  border-radius: 15px;
+  padding: 40px 200px;
+  margin: 20px 0;
+  position: relative;
+`;
+
+export default Card;
