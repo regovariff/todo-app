fdd5df7ca68dc6b0e798dde5dc632dfeb398208d
Ariff <ariff@regovtech.com>
2023-09-13 11:42:29 +0800

    basic profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,49 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Button from "../components/Button";
 
 function Profile() {
+  const [userData, setUserData] = useState({ name: "", email: "" });
 
-    return (
-        <>
-        <h1>Profile</h1>
-        </>
-    );
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    // localStorage.removeItem("loggedUser");
+    navigate("/");
+  };
+
+  useEffect(() => {
+    // Retrieve user data from local storage
+    const loggedUser = localStorage.getItem("loggedUser");
+
+    if (loggedUser) {
+      const thisLoggedUser = JSON.parse(loggedUser);
+
+      if (thisLoggedUser) {
+        setUserData({ name: thisLoggedUser.name, email: thisLoggedUser.email });
+      } else {
+        console.log("Invalid user");
+      }
+    } else {
+      console.log("No data");
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <Button
+        onClick={onLogout}
+      >
+        Logout
+      </Button>
+      <br />
+      <div>
+        <h2>Name: {userData.name}</h2>
+        <h2>Email: {userData.email}</h2>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
